cac318958bbd5da77d0fcd8e1bf163b594f3751a
Danil Antoshenkov <adloky@gmail.com>
2023-09-13 18:39:42 +0500

    chess-graph

---
+++ chess-com-pgn.ps1
@@ -0,0 +1,13 @@
+$User= Read-Host -Prompt "Which chess.com user would you like to download archives for?"
+$Folder="d:"
+IF (!(Test-Path $folder))
+    {New-Item -ItemType  Directory -Path $folder}
+Write-Output "Archive files will be saved to $($Folder)"
+$Response=Invoke-RestMethod -uri "https://api.chess.com/pub/player/$($User)/games/archives"
+
+$PGN=@()
+foreach ($Month in $Response.archives)
+    {
+    $PGN+=(Invoke-RestMethod -uri "$Month").Games.pgn}
+    ($PGN | ForEach-Object {$_ + "`r`n"}) |
+    Out-File -FilePath $folder\$User.pgn -Encoding ascii
