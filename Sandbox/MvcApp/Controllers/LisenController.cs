6c1cbdfc1c5bc8a262d7382bdf839b3a08c89aa0
Danil Antoshenkov <adloky@gmail.com>
2024-10-09 14:09:39 +0500

    lisen.html

---
+++ Sandbox/MvcApp/Controllers/LisenController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using System.Text;
+
+namespace MvcApp.Controllers
+{
+    public class LisenController : ApiController
+    {
+        static string[] vals = File.ReadAllLines(@"d:\Projects\smalls\lisen.txt");
+        static Random rnd = new Random();
+
+        [HttpPost]
+        [Route("api/lisen/next")]
+        public HttpResponseMessage LastBackup() {
+            var val = vals[rnd.Next(vals.Length)];
+            return new HttpResponseMessage() {
+                Content = new StringContent(val, Encoding.UTF8, "text/html")
+            };
+        }
+    }
+}
