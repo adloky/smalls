61301d7faa9d720676310e83febed8e92c51de69
Danil Antoshenkov <adloky@gmail.com>
2025-09-22 12:24:37 +0500

    pron-text

--- Sandbox/MvcApp/Controllers/HomeController.cs
+++ Sandbox/MvcApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace MvcApp.Controllers {
     public class HomeController : Controller {
@@ -10,6 +11,14 @@ namespace MvcApp.Controllers {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        public ActionResult Text() {
+            var ss = System.IO.File.ReadAllLines(@"d:\Projects\smalls\bins\con-ru.txt");
+            var rnd = new Random();
+            var rs = Enumerable.Range(0, 10).Select(x => ss[rnd.Next(ss.Length)]).Select(x => x.Split('|')).ToList();
+
+            return View(rs.Select(x => x[0]).Union(rs.Select(x => x[1])));
         }
     }
 }
