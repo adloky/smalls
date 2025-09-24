6001036e8a5faaf6396a85ef3276e73fa77be44e
Danil Antoshenkov <adloky@gmail.com>
2025-09-24 10:20:56 +0500

    freq-group-optimization

--- Sandbox/SandboxLib/SandboxConfig.cs
+++ Sandbox/SandboxLib/SandboxConfig.cs
@@ -9,17 +9,20 @@ namespace Sandbox {
     public class SandboxConfig {
         static Dictionary<string, string> _default;
 
-        static Dictionary<string, string> getDefault() {
+        public static void Reread() {
             var configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ".sandbox");
             configPath = File.Exists(configPath) ? configPath : @"d:/.sandbox";
-            return File.ReadAllLines(configPath).Where(x => !x.StartsWith("//")).Select(x => {
+            _default = File.ReadAllLines(configPath).Where(x => !x.StartsWith("//")).Select(x => {
                 var i = x.IndexOf(":");
                 return new KeyValuePair<string, string>(x.Substring(0, i), x.Substring(i + 1).Trim());
             }).ToDictionary(x => x.Key, x => x.Value);
         }
 
         public static Dictionary<string, string> Default {
-            get => _default ?? (_default = getDefault());
+            get {
+                if (_default == null) Reread();
+                return _default;
+            }
         }
     }
 }
